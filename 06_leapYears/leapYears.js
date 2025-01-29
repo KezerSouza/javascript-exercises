4eb2d2eec0c086036d79b2fbc5c84adf33a710ae
Kezer <kezer@discente.ufg.br>
2025-01-28 22:52:35 -0300

    Solved exercise 6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,11 @@
-const leapYears = function() {
+function leapYears(x) {
 
-};
+    if(x % 400 == 0 && x % 4 == 0) return true;
+    if(x % 100 == 0) return false;
+    if(x % 4 == 0) return true;
+    return false;
+
+}
 
 // Do not edit below this line
 module.exports = leapYears;
