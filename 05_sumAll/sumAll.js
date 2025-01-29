7042f4a0af682f2cd03262fc4819f1da1206bcec
Kezer <kezer@discente.ufg.br>
2025-01-28 22:44:47 -0300

    Solved exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,16 @@
-const sumAll = function() {
+function sumAll(x, y) {
+    soma = 0;
 
-};
+    if(x < 0 || y < 0) return "ERROR";
+    if(x % 1 != 0 || y % 1 != 0) return "ERROR";
+    if(typeof(x) != 'number' || typeof(y) != 'number') return "ERROR";
+
+    for(i = Math.min(x, y); i <= Math.max(x, y); i++) {
+        soma += i;
+    }
+
+    return soma;
+}
 
 // Do not edit below this line
 module.exports = sumAll;
