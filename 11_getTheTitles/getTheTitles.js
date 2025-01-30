31d15a48c74b9043f081207f38dfde5d88c02678
Kezer <kezer@discente.ufg.br>
2025-01-30 19:47:20 -0300

    Solved exercise 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,12 @@
-const getTheTitles = function() {
+function getTheTitles(x) {
+    titles = [];
 
-};
+    for(let i = 0; i < x.length; i++) {
+        titles.push(x[i].title);
+    }
+
+    return titles;
+}
 
 // Do not edit below this line
 module.exports = getTheTitles;
