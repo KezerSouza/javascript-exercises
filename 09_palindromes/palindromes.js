2913cfb7fd9a6cc3330767f37d21c24eec7ad1e2
Kezer <kezer@discente.ufg.br>
2025-01-30 10:42:35 -0300

    Solved exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,20 @@
-const palindromes = function () {
+function palindromes(x) {
+    x = x.replaceAll(" ", "");
+    x = x.replaceAll(",", "");
+    x = x.replaceAll(".", "");
+    x = x.replaceAll("!", "");
+    x = x.toLowerCase();
 
-};
+    reverse = "";
+    for(let i = x.length-1; i >= 0; i--) {
+        reverse = reverse.concat(x[i]);
+    }
+
+    let result = false;
+    if(reverse == x) result = true;
+
+    return result;
+}
 
 // Do not edit below this line
 module.exports = palindromes;
