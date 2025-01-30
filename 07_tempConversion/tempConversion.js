c7b3ba753e5b13fe29a22167152ef236395fccdb
Kezer <kezer@discente.ufg.br>
2025-01-30 10:17:53 -0300

    Solved exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
-};
+function convertToCelsius(temperature) {
+  temperature = (temperature - 32) * 5/9;
+  if(temperature % 1 != 0) temperature = Number(temperature.toFixed(1));
 
-const convertToFahrenheit = function() {
+  return temperature;
+}
+
+const convertToFahrenheit = function(temperature) {
+  temperature = temperature * 9/5 + 32;
+  if(temperature % 1 != 0) temperature = Number(temperature.toFixed(1));
+
+  return temperature;
 };
 
 // Do not edit below this line
