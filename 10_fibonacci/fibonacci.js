45e5ed844ec73ed5031910b9ddd6a332b63a92d7
Kezer <kezer@discente.ufg.br>
2025-01-30 10:54:37 -0300

    Solved exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,17 @@
-const fibonacci = function() {
+function fibonacci(x) {
+    // 1, 1, 2, 3, 5, 8
+    let sequencia = [1, 1, 2];
 
-};
+    for(let i = 3; i < x; i++) {
+        sequencia.push(sequencia[i-1] + sequencia[i-2]);
+    }
+
+    if(x == 0) return 0;
+    if(x < 0) return "OOPS";
+
+    return sequencia[x-1];
+
+}
 
 // Do not edit below this line
 module.exports = fibonacci;
