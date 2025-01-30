8d67591f8347acb218382171fa76b684e7e1df54
Kezer <kezer@discente.ufg.br>
2025-01-30 20:17:51 -0300

    Solved exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,11 @@
-const findTheOldest = function() {
+function findTheOldest(x) {
+    let ages = x.map(person => (person.yearOfDeath ? person.yearOfDeath : 2025) - person.yearOfBirth);
 
-};
+    let max = Math.max(...ages);
+    let max_index = ages.indexOf(max);
+
+    return x[max_index];
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
--- 12_findTheOldest/findTheOldest.spec.js
+++ 12_findTheOldest/findTheOldest.spec.js
@@ -2,26 +2,26 @@ const findTheOldest = require('./findTheOldest')
 
 describe('findTheOldest', () => {
   test('finds the person with the greatest age!', () => {
-    const people = [
-      {
-        name: "Carly",
-        yearOfBirth: 1942,
-        yearOfDeath: 1970,
-      },
-      {
-        name: "Ray",
-        yearOfBirth: 1962,
-        yearOfDeath: 2011,
-      },
-      {
-        name: "Jane",
-        yearOfBirth: 1912,
-        yearOfDeath: 1941,
-      },
-    ]
+      const people = [
+        {
+          name: "Carly",
+          yearOfBirth: 1942,
+          yearOfDeath: 1970,
+        },
+        {
+          name: "Ray",
+          yearOfBirth: 1962,
+          yearOfDeath: 2011,
+        },
+        {
+          name: "Jane",
+          yearOfBirth: 1912,
+          yearOfDeath: 1941,
+        },
+      ]
     expect(findTheOldest(people).name).toBe('Ray');
   });
-  test.skip('finds the oldest person if yearOfDeath field is undefined on a non-oldest person', () => {
+  test('finds the oldest person if yearOfDeath field is undefined on a non-oldest person', () => {
     const people = [
       {
         name: "Carly",
@@ -40,7 +40,7 @@ describe('findTheOldest', () => {
     ]
     expect(findTheOldest(people).name).toBe('Ray');
   });
-  test.skip('finds the oldest person if yearOfDeath field is undefined for the oldest person', () => {
+  test('finds the oldest person if yearOfDeath field is undefined for the oldest person', () => {
     const people = [
       {
         name: "Carly",
